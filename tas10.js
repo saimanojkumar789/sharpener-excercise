5e6b8a312aa1b505772abac1a8dde13d90e7ab8d
872677 <saimanojkumar789@gmail.com>
2023-02-21 12:36:19 +0530

    task 11

--- tas10.js
+++ tas10.js
@@ -4,8 +4,12 @@ form.addEventListener('submit',onsubmit);
 
 function onsubmit(e){
     e.preventDefault;
-    localStorage.setItem('firstname',e.target.firstname.value);
-    localStorage.setItem('lastname',e.target.lastname.value);
-    localStorage.setItem('phonenumber',e.target.mobilenumber.value);
-    localStorage.setItem('email',e.target.email.value);
+    const myobj = {
+        name : e.target.firstname.value,
+        lastname : e.target.lastname.value,
+        mobilenumber : e.target.mobilenumber.value,
+        email : e.target.email.value
+    }
+    myobj_serialized = JSON.stringify(myobj);
+    localStorage.setItem('myobj',myobj_serialized);
 }
