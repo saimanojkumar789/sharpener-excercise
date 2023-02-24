e0c9603def7b86147e74a6d619e46da8e5ce18c8
872677 <saimanojkumar789@gmail.com>
2023-02-24 18:47:47 +0530

    task 13

--- tas10.js
+++ tas10.js
@@ -1,9 +1,10 @@
-var form = document.querySelector('.form-control');
+var form = document.querySelector('#addForm');
+var items = document.getElementById('items');
+form.addEventListener('submit',onSubmit);
+items.addEventListener('click',onClick);
 
-form.addEventListener('submit',onsubmit);
-
-function onsubmit(e){
-    e.preventDefault;
+function onSubmit(e){
+    e.preventDefault();
     const myobj = {
         name : e.target.firstname.value,
         lastname : e.target.lastname.value,
@@ -12,4 +13,25 @@ function onsubmit(e){
     }
     myobj_serialized = JSON.stringify(myobj);
     localStorage.setItem(myobj.email,myobj_serialized);
+    var li = document.createElement('li');
+    li.className="list-group-item"
+    li.appendChild(document.createTextNode(`${myobj.name}   ${myobj.lastname}     ${myobj.mobilenumber}     ${myobj.email}`));
+    var btn = document.createElement('button');
+    btn.className=" delete";
+    btn.style="float:right";
+    btn.appendChild(document.createTextNode("delete"));
+    li.appendChild(btn);
+    items.appendChild(li);
+}
+
+function onClick(e){
+    e.preventDefault();
+    if(e.target.classList.contains('delete')){
+        if(confirm('Are you sure?')){
+            var li = e.target.parentElement;
+            var email = li.textContent.split("   ")[3];
+            localStorage.removeItem(email);
+            items.removeChild(li);
+        }
+    }
 }
